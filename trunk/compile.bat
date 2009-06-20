d40296599db04bb3f103cd729db2b68f01510469
bharrisau <bharrisau@7af992c4-5d33-11de-992f-fdd7e25b9163>
2009-06-20 02:18:59 +0000

    Compile script update

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -1,2 +1,2 @@
-del server.exe
-c:\windows\microsoft.net\Framework\v2.0.50727\csc.exe /unsafe /out:Server.exe /recurse:server\*.cs /win32icon:Server\runuo.ico
+If EXIST server.exe del server.exe
+%WINDIR%\microsoft.net\Framework\v2.0.50727\csc.exe /unsafe /out:Server.exe /recurse:server\*.cs /win32icon:Server\runuo.ico
