264cd82a48cc905f87b0311100b2047d5c3c8c27
bharrisau <bharrisau@7af992c4-5d33-11de-992f-fdd7e25b9163>
2009-06-23 12:13:01 +0000

    Better resgate for testing

--- trunk/Scripts/Items/Misc/ResGate.cs
+++ trunk/Scripts/Items/Misc/ResGate.cs
@@ -5,9 +5,18 @@ namespace Server.Items
 {
 	public class ResGate : Item
 	{
+		private bool m_refresh;
+		
 		public override string DefaultName
 		{
 			get { return "a resurrection gate"; }
+		}
+		
+		[CommandProperty( AccessLevel.GameMaster )]
+		public bool Refresh
+		{
+			get{ return m_refresh; }
+			set{ m_refresh = value;}
 		}
 
 		[Constructable]
@@ -24,17 +33,28 @@ namespace Server.Items
 
 		public override bool OnMoveOver( Mobile m )
 		{
-			if ( !m.Alive && m.Map != null && m.Map.CanFit( m.Location, 16, false, false ) )
-			{
+			if(m_refresh) {
 				m.PlaySound( 0x214 );
 				m.FixedEffect( 0x376A, 10, 16 );
 
-				m.CloseGump( typeof( ResurrectGump ) );
-				m.SendGump( new ResurrectGump( m ) );
-			}
-			else
-			{
-				m.SendLocalizedMessage( 502391 ); // Thou can not be resurrected there!
+				m.Resurrect();
+				
+				m.Hits = m.HitsMax;
+				m.Stam = m.Dex;
+				m.Mana = m.Int;
+			} else {
+				if ( !m.Alive && m.Map != null && m.Map.CanFit( m.Location, 16, false, false ) )
+				{
+					m.PlaySound( 0x214 );
+					m.FixedEffect( 0x376A, 10, 16 );
+	
+					m.CloseGump( typeof( ResurrectGump ) );
+					m.SendGump( new ResurrectGump( m ) );
+				}
+				else
+				{
+					m.SendLocalizedMessage( 502391 ); // Thou can not be resurrected there!
+				}
 			}
 
 			return false;
@@ -44,7 +64,8 @@ namespace Server.Items
 		{
 			base.Serialize( writer );
 
-			writer.Write( (int) 0 ); // version
+			writer.Write( (int) 1 ); // version
+			writer.Write( m_refresh );
 		}
 
 		public override void Deserialize( GenericReader reader )
@@ -52,6 +73,9 @@ namespace Server.Items
 			base.Deserialize( reader );
 
 			int version = reader.ReadInt();
+			if(version > 0) {
+				m_refresh = reader.ReadBool();
+			}
 		}
 	}
 }
