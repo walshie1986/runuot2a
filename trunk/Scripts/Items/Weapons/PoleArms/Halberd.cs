b50f3dabe6881c1e0c2b8a150335958d130868b0
bharrisau <bharrisau@7af992c4-5d33-11de-992f-fdd7e25b9163>
2009-06-24 13:24:00 +0000

    Dices for Halberd

--- trunk/Scripts/Items/Weapons/PoleArms/Halberd.cs
+++ trunk/Scripts/Items/Weapons/PoleArms/Halberd.cs
@@ -19,6 +19,10 @@ namespace Server.Items
 		public override int OldMinDamage{ get{ return 5; } }
 		public override int OldMaxDamage{ get{ return 49; } }
 		public override int OldSpeed{ get{ return 25; } }
+		
+		public override int DiceRolls{ get{ return 2; } }
+		public override int DiceSides{ get{ return 23; } }
+		public override int DiceOffset{ get{ return 3; } }
 
 		public override int InitMinHits{ get{ return 31; } }
 		public override int InitMaxHits{ get{ return 80; } }
